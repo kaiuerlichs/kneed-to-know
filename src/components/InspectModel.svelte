069c3cbe63b7c567daeeacc4034c87c6eafdf93f
Kai Uerlichs <2421101@dundee.ac.uk>
2022-03-22 04:07:10 +0000

    button toggle yay

--- src/components/InspectModel.svelte
+++ src/components/InspectModel.svelte
@@ -65,7 +65,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
-            <div on:click={() => {showLayersMenu = true;}}>
+            <div on:click={() => {showLayersMenu = showLayersMenu ? false : true;}}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
