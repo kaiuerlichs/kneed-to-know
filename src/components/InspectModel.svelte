9446dd54daeb6e644ee5a20675b91aad10bc4cb0
Kai Uerlichs <2421101@dundee.ac.uk>
2022-03-23 13:37:14 +0000

    go go go

--- src/components/InspectModel.svelte
+++ src/components/InspectModel.svelte
@@ -10,12 +10,14 @@
 
     let ready = false;
 
-
     let showLayersMenu = false;
     let layerSkinActive = true;
     let layerBoneActive = true;
     let layerTendActive = true;
 
+    let currentLandmark = "";
+    let layerType = "";
+    let showLandmarkPopup = false;
 
     function closeView(){
         dispatch("close")
@@ -26,8 +28,49 @@
         target.postMessage(sig, window.origin)
     }
 
+    function closePopup(){
+        showLandmarkPopup = false;
+        currentLandmark = ""
+        sendSignal("landmarks;reset")
+    }
+
     window.addEventListener("nodesReady", () => {
         ready = true;
+    })
+
+    window.addEventListener("message", (param) => {
+        let message = param.data.split(";")
+        fetch("/med_data/landmarks.json")
+        .then(response => response.text())
+        .then((data) => {
+            let landmarks = JSON.parse(data)
+
+            let skin = landmarks.layers.skin 
+            skin.forEach(el => {
+                if(el.id == message[2]){
+                    currentLandmark = el.name
+                    layerType = "skin"
+                }
+            })
+            let bone = landmarks.layers.bone 
+            bone.forEach(el => {
+                if(el.id == message[2]){
+                    currentLandmark = el.name
+                    layerType = "bone"
+                }
+            })
+            let tendon = landmarks.layers.tendon
+            tendon.forEach(el => {
+                if(el.id == message[2]){
+                    currentLandmark = el.name
+                    layerType = "tendon"
+                }
+            })
+
+            if(currentLandmark != ""){
+                showLandmarkPopup = true;
+            }
+        })
     })
 </script>
 
@@ -84,6 +127,45 @@
                 <button on:click={() => {sendSignal("layer;bone;hide");}}>Hide Bone</button><br/>
                 <button on:click={() => {sendSignal("layer;tendon;show");}}>Show Tendon</button><br/>
                 <button on:click={() => {sendSignal("layer;tendon;hide");}}>Hide Tendon</button>
+            </div>
+        </div>
+    {/if}
+
+    {#if showLandmarkPopup}
+        <div class="invisible lg:visible">
+            <div class="absolute bottom-10 left-10 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-1/3">
+                <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[20px] z-20 clickable my-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                </div>
+                <span class="text-xl mx-5 my-auto">
+                    {currentLandmark}
+                </span>
+                <div class="text-white bg-white/30 rounded-[12px] z-20 clickable my-auto ml-auto justify-self-end" on:click={closePopup}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-2" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="visible lg:invisible">
+            <div class="w-3/4 h-full mx-auto">
+                <div class="fixed bottom-32 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-3/4 mx-auto">
+                    <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[12px] z-20 clickable my-auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                          </svg>
+                    </div>
+                    <span class="text-lg mx-5 my-auto">
+                        {currentLandmark}
+                    </span>
+                    <div class="text-white rounded-[12px] bg-white/30 z-20 clickable my-auto ml-auto justify-self-end" on:click={closePopup}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     {/if}
