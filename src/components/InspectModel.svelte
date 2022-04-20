3e416217b943b6bf366890336624338d8bcc2c7b
Kai Uerlichs <2421101@dundee.ac.uk>
2022-04-20 13:48:40 +0200

    Additional stuff for inspect view

--- src/components/InspectModel.svelte
+++ src/components/InspectModel.svelte
@@ -1,44 +1,92 @@
 <script>
+    // Import title bar component and svelte dependencies
     import TitleBar from "./TitleBar.svelte"
-
     import { fade, blur } from 'svelte/transition';
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
 
+    // Component config
     export let config = "";
     config = config;
 
+    // AR ready guard
     let ready = false;
 
+    // Layer activity variables
     let showLayersMenu = false;
     let layerSkinActive = true;
     let layerBoneActive = true;
     let layerTendActive = true;
+    let showLandmarks = true;
 
+    $: {
+        if(ready){
+            if(layerSkinActive){
+                sendSignal("layer;skin;show");
+            }
+            else{
+                sendSignal("layer;skin;hide");
+            }
+
+            if(layerBoneActive){
+                sendSignal("layer;bone;show");
+            }
+            else{
+                sendSignal("layer;bone;hide");
+            }
+
+            if(layerTendActive){
+                sendSignal("layer;tendon;show");
+            }
+            else{
+                sendSignal("layer;tendon;hide");
+            }
+        }
+    }
+
+    // Landmark popup variables
     let currentLandmark = "";
+    let landmarkDesc = "";
     let layerType = "";
     let showLandmarkPopup = false;
+    let showLandmarkInfo = false;
 
+    // Function to dispatch close event
     function closeView(){
         dispatch("close")
     }
 
+    function toggleLandmarks(){
+        if(showLandmarks){
+            showLandmarks = false;
+            sendSignal("landmark;hide");
+        }
+        else{
+            showLandmarks = true;
+            sendSignal("landmark;show");
+        }
+    }
+
+    // Function to send signal to AR frame
     function sendSignal(sig){
         let target = document.getElementById("sceneFrame").contentWindow
         target.postMessage(sig, window.origin)
     }
 
+    // Function to dismiss popup
     function closePopup(){
         showLandmarkPopup = false;
-        currentLandmark = ""
-        sendSignal("landmarks;reset")
+        sendSignal("landmark;reset")
     }
 
+    // Function to handle ready event from AR scene
     window.addEventListener("nodesReady", () => {
         ready = true;
     })
 
+    // Function to handle signal from AR frame
     window.addEventListener("message", (param) => {
+        // Find correct landmark in json database
         let message = param.data.split(";")
         fetch("/med_data/landmarks.json")
         .then(response => response.text())
@@ -49,6 +97,7 @@
             skin.forEach(el => {
                 if(el.id == message[2]){
                     currentLandmark = el.name
+                    landmarkDesc = el.desc
                     layerType = "skin"
                 }
             })
@@ -56,6 +105,7 @@
             bone.forEach(el => {
                 if(el.id == message[2]){
                     currentLandmark = el.name
+                    landmarkDesc = el.desc
                     layerType = "bone"
                 }
             })
@@ -63,6 +113,7 @@
             tendon.forEach(el => {
                 if(el.id == message[2]){
                     currentLandmark = el.name
+                    landmarkDesc = el.desc
                     layerType = "tendon"
                 }
             })
@@ -75,6 +126,8 @@
 </script>
 
 <div class="h-full w-full relative bg-black">
+
+    <!-- Display laoding message while AR scene is initialising -->
     {#if !ready}
         <div class="absolute h-full w-full flex items-center justify-center" transition:fade>
             <div class="backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5">
@@ -83,6 +136,7 @@
         </div>
     {/if}
 
+    <!-- Embed AR scene as iframe -->
     <iframe
         id="sceneFrame"
         src="./scene.html"
@@ -91,8 +145,9 @@
         class="absolute h-full w-full"
     />
 
+    <!-- Insert DOM overlay -->
     {#if ready}
-        <div class="absolute top-10 right-10 backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20" on:click={closeView}>
+        <div class="cursor-pointer absolute top-10 right-10 backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20" on:click={closeView}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
             </svg>
@@ -104,11 +159,19 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-            <div on:click={() => {showLayersMenu = showLayersMenu ? false : true;}}>
+            <div on:click={toggleLandmarks}>
+                {#if showLandmarks}
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+                {:else}
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                    </svg>
+                {/if}
+            </div>
+            <div on:click={() => {showLayersMenu = showLayersMenu ? false : true;}} class="cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
@@ -116,25 +179,47 @@
         </div>
     {/if}
 
+    <!-- Layers menu code -->
     {#if showLayersMenu}
         <div class="absolute w-full lg:w-5/12 h-full z-40 flex flex-col" transition:fade>
             <div class="m-10 h-full p-10 bg-white rounded-[20px] drop-shadow-lg">
                 <TitleBar text="Controls" on:close={() => {showLayersMenu=false;}} />
-                <h1 class="heading text-4xl">Layers</h1>
-                <button on:click={() => {sendSignal("layer;skin;show");}}>Show Skin</button><br/>
-                <button on:click={() => {sendSignal("layer;skin;hide");}}>Hide Skin</button><br/>
-                <button on:click={() => {sendSignal("layer;bone;show");}}>Show Bone</button><br/>
-                <button on:click={() => {sendSignal("layer;bone;hide");}}>Hide Bone</button><br/>
-                <button on:click={() => {sendSignal("layer;tendon;show");}}>Show Tendon</button><br/>
-                <button on:click={() => {sendSignal("layer;tendon;hide");}}>Hide Tendon</button>
+                <h1 class="heading text-4xl">Toggle Layers</h1>
+                <label>
+                    <input type="checkbox" bind:checked={layerSkinActive}>
+                    Skin
+                </label>
+                <br>
+                <label>
+                    <input type="checkbox" bind:checked={layerBoneActive}>
+                    Bone
+                </label>
+                <br>
+                <label>
+                    <input type="checkbox" bind:checked={layerTendActive}>
+                    Tendons and Ligaments
+                </label>
             </div>
         </div>
     {/if}
 
+    <!-- Landmark info code -->
+    {#if showLandmarkInfo}
+        <div class="absolute w-full lg:w-5/12 h-full z-40 flex flex-col" transition:fade>
+            <div class="m-10 h-full p-10 bg-white rounded-[20px] drop-shadow-lg">
+                <TitleBar text={currentLandmark} on:close={() => {showLandmarkInfo=false;}} />
+                <p>
+                    {landmarkDesc}
+                </p>
+            </div>
+        </div>
+    {/if}
+
+    <!-- Landmark Popup code -->
     {#if showLandmarkPopup}
         <div class="invisible lg:visible" transition:fade>
-            <div class="absolute bottom-10 left-10 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-1/3">
-                <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[20px] z-20 clickable my-auto">
+            <div class="absolute bottom-10 left-10 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-1/3" on:click={() => {showLandmarkInfo=true;}}>
+                <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[20px] z-20 cursor-pointer my-auto">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
@@ -142,7 +227,7 @@
                 <span class="text-xl mx-5 my-auto">
                     {currentLandmark}
                 </span>
-                <div class="text-white bg-white/30 rounded-[12px] z-20 clickable my-auto ml-auto justify-self-end" on:click={closePopup}>
+                <div class="text-white bg-white/30 rounded-[12px] z-30 cursor-pointer my-auto ml-auto justify-self-end" on:click|stopPropagation={closePopup}>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-2" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
                     </svg>
@@ -151,8 +236,8 @@
         </div>
         <div class="visible lg:invisible" transition:fade>
             <div class="w-3/4 h-full mx-auto">
-                <div class="fixed bottom-32 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-3/4 mx-auto">
-                    <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[12px] z-20 clickable my-auto">
+                <div class="fixed bottom-32 flex flex-row backdrop-blur-sm bg-white/30 text-white rounded-[20px] z-20 p-5 align-middle w-3/4 mx-auto" on:click={() => {showLandmarkInfo=true;}}>
+                    <div class="backdrop-blur-sm bg-[#D2FCAC] text-black rounded-[12px] z-20 cursor-pointer my-auto">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                           </svg>
@@ -160,7 +245,7 @@
                     <span class="text-lg mx-5 my-auto">
                         {currentLandmark}
                     </span>
-                    <div class="text-white rounded-[12px] bg-white/30 z-20 clickable my-auto ml-auto justify-self-end" on:click={closePopup}>
+                    <div class="text-white rounded-[12px] bg-white/30 z-30 cursor-pointer my-auto ml-auto justify-self-end" on:click|stopPropagation={closePopup}>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-2" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
                         </svg>
